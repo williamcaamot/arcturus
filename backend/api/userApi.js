86a4df8f30f1b4402d76e27dab696e86c150ea24
williamdev <william-ca@live.no>
2024-05-27 14:37:30 +0200

    finish setup of local dev environment

--- backend/api/userApi.js
+++ backend/api/userApi.js
@@ -1,1 +1,13 @@
-console.log("The user is always right");
+import express from "express";
+
+export function userAPI(mongoClient){
+    const router = express.Router();
+
+    router.get("", async(req, res)=>{
+        res.json({message:"Hello world"});
+    })
+
+    return router;
+
+
+}
--- backend/server.js
+++ backend/server.js
@@ -3,40 +3,48 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import * as path from "path";
 import { createProxyMiddleware} from "http-proxy-middleware";
+import {userAPI} from "./api/userApi.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 const mongoClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
 
 
+app.use("/api/v1/user", userAPI(mongoClient))
 
+
+
+// Middleware to handle requests that are not API relevant (keep this at bottom, want to first handle anything that has to do with API)
 if (process.env.NODE_ENV === 'production') {
     // Serve static files from the Vite build directory in production
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     });
 } else {
     // Proxy to Vite dev server in development
-    console.log("Proxying request to vite frontend")
+    console.log("Proxying request to vite frontend");
     app.use(
         '/',
         createProxyMiddleware({
             target: 'http://localhost:5173',
             changeOrigin: true,
+            ws: true,
         })
     );
 }
 
 
+// Connect to MongoDB
 mongoClient.connect()
     .then(() => console.log('Connected successfully to MongoDB'))
     .catch(err => console.error('Connection to MongoDB failed', err))
     .finally(() => mongoClient.close());
 
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
+// Run server
+app.listen(process.env.PORT || port, () => {
+    console.log("Server is running on port " + process.env.PORT || port);
 }) ;
