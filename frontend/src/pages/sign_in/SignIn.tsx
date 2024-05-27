9aeaf770d65b0316477ad41fa41bde775bb05367
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-27 14:55:17 +0200

    signin skeleton

--- frontend/src/pages/sign_in/SignIn.tsx
+++ frontend/src/pages/sign_in/SignIn.tsx
@@ -1,9 +1,28 @@
-
+import "./styles/signIn.css";
 
 const SignIn = () => {
     return (
-        <div>
-          Test
+        <div className="signInContainer">
+            <div className="signInLogo">
+                <h1>LOGO HERE</h1>
+                <br />
+                <h2 style={{ margin: 0 }}>Welcome Darwin</h2>
+            </div>
+            <div className="signInBoxes">
+                <p>Facebook</p>
+            </div>
+            <div className="signInBoxes">
+                <p>Microsoft</p>
+            </div>
+            <div className="signInBoxes">
+                <p>Google</p>
+            </div>
+            <div className="signInBoxes">
+                <p>Apple</p>
+            </div>
+            <div className="signInBoxes">
+                <p>Email</p>
+            </div>
         </div>
     );
 };
