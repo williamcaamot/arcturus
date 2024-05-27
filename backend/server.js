6d2f336ccf757f425c21a85e6139c120897e0b83
williamdev <william-ca@live.no>
2024-05-27 14:26:03 +0200

    Setup of local dev environment with backend serving frontend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,40 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import * as path from "path";
+import { createProxyMiddleware} from "http-proxy-middleware";
 
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
+const mongoClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
 
-const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
 
-client.connect()
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the Vite build directory in production
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+    });
+} else {
+    // Proxy to Vite dev server in development
+    console.log("Proxying request to vite frontend")
+    app.use(
+        '/',
+        createProxyMiddleware({
+            target: 'http://localhost:5173',
+            changeOrigin: true,
+        })
+    );
+}
+
+
+mongoClient.connect()
     .then(() => console.log('Connected successfully to MongoDB'))
     .catch(err => console.error('Connection to MongoDB failed', err))
-    .finally(() => client.close());
+    .finally(() => mongoClient.close());
 
 
 app.listen(PORT, () => {
