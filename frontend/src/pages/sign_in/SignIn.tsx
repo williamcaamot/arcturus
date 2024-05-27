cb6cceb60607be470f59a87f8dd2dd995e19b881
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-27 17:39:11 +0200

    Logo header and buttons now has colors and icons

--- frontend/src/pages/sign_in/SignIn.tsx
+++ frontend/src/pages/sign_in/SignIn.tsx
@@ -1,4 +1,9 @@
 import "./styles/signIn.css";
+import appleLogo from '../../../public/images/appleLogo3.png';
+import facebookLogo from '../../../public/images/facebookLogo.png';
+import googleLogo from '../../../public/images/googleLogo.png';
+import userIcon from '../../../public/images/userIcon2.png';
+
 
 const SignIn = () => {
     return (
@@ -12,19 +17,23 @@ const SignIn = () => {
                <img src="" alt="" className="signInLogo" />
             </div>
 
-            <button className="signInBoxes">
-              <p>Continue with Apple</p>
+            <button className="signInBoxes appleBtn">
+                <img src={appleLogo} alt="Apple logo" className="logo" />
+                <p>Continue with Apple</p>
             </button>
 
-            <button className="signInBoxes">
+            <button className="signInBoxes facebookBtn">
+                <img src={facebookLogo} alt="Facebook logo" className="logo" />
                 <p>Continue with Facebook</p>
             </button>
 
-            <button className="signInBoxes">
+            <button className="signInBoxes googleBtn">
+                <img src={googleLogo} alt="Google logo"  className="logo"/>
                 <p>Continue with Google</p>
             </button>
 
-            <button className="signInBoxes">
+            <button className="signInBoxes newUserBtn">
+            <img src={userIcon} alt="Google logo"  className="logo"/>
                 <p>Register new user</p>
             </button>
 
