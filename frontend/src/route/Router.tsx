87f54595dd6c9114efd0f45820eeb884ae758355
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-27 13:56:39 +0200

    added notfoundpage and index

--- frontend/src/route/Router.tsx
+++ frontend/src/route/Router.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/landingpage/LandingPage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 
 
 const Router = () => {
     return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-         
-        </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
