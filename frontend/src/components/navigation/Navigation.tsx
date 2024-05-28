dcfb549ecdcb9348ffa73889e84ccd1a117a8071
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-29 00:04:50 +0200

    added some changes to make the content go behind nav bar, also added icons and made it go all the way to the right

--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './styles/navigation.css';
+import navProfile from "../../../public/images/navProfile.png";
+import navHome from "../../../public/images/navHome.png";
+import navStats from "../../../public/images/navStats.png";
+import navWorkouts from "../../../public/images/navWorkouts.png";
+
 
 const Navigation = () => {
     const navigateTo = (path: string) => {
@@ -8,11 +13,23 @@ const Navigation = () => {
   
     return (
       <div className="navigation">
-        <button className="nav-button" onClick={() => navigateTo('/')}>Home</button>
+        <button className="nav-button" onClick={() => navigateTo('/')}>
+          <img src={navHome} alt="Home"  />
+        </button>
+
+        <button className="nav-button" onClick={() => navigateTo('/statistics')}>
+          <img src={navStats} alt="Stats" />
+        </button>
+        
         <button className="nav-button" onClick={() => navigateTo('/SignIn')}>Sign In</button>
-        <button className="nav-button" onClick={() => navigateTo('/Statistics')}>Stats</button>
-        <button className="nav-button" onClick={() => navigateTo('/Test3')}>Test3</button>
-        <button className="nav-button" onClick={() => navigateTo('/MyProfile')}>My Profile</button>
+        
+        <button className="nav-button" onClick={() => navigateTo('')}>
+          <img src={navWorkouts} alt="Workouts" />
+        </button>
+        
+        <button className="nav-button" onClick={() => navigateTo('/MyProfile')}>
+          <img src={navProfile} alt="Profile" />
+        </button>
       </div>
     );
   };
