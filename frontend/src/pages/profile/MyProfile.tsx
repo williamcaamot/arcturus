733e70feada204063372bc378074c5b76e8d2b07
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-28 20:28:29 +0200

    almost finished my profile page

--- frontend/src/pages/profile/MyProfile.tsx
+++ frontend/src/pages/profile/MyProfile.tsx
@@ -2,6 +2,16 @@ import React, { useEffect } from "react";
 import "./styles/myProfile.css";
 import backArrow from "../../../public/images/backArrow.png";
 import WorkoutService from "../../services/WorkoutService.ts";
+import statsIcon from "../../../public/images/statsIcon.png";
+import accountSettingsIcon from "../../../public/images/settingsicon.png";
+import remindersIcon from "../../../public/images/reminderIcon.png";
+import privacyPolicyIcon from "../../../public/images/policyIcon.png";
+import tCIcon from "../../../public/images/termsandcontitionsIcon.png";
+import updatesIcon from "../../../public/images/updatesIcon.png";
+import notificationsIcon from "../../../public/images/notificationsIcon.png";
+import timeSpentIcon from "../../../public/images/highlightTimeIcon.png";
+import kgsLiftedIcon from "../../../public/images/HighlightKgIcon.png";
+import calsBurnedIcon from "../../../public/images/highlightCalsIcon.png";
 
 
 const Statistics = () => {
@@ -68,6 +78,8 @@ const Statistics = () => {
                                     stroke-width="3"
                                 />
                             </svg>
+                            <img src={timeSpentIcon} alt="timeSpentIcon" />
+
                             <h4>TIME SPENT</h4>
                             <p>46 hrs 12 min</p>
                         </div>
@@ -88,8 +100,10 @@ const Statistics = () => {
                                     stroke-width="3"
                                 />
                             </svg>
-                            <h4>TIME SPENT</h4>
-                            <p>46 hrs 12 min</p>
+                            <img src={kgsLiftedIcon} alt="kgsLiftedIcon" />
+
+                            <h4>KGS LIFTED</h4>
+                            <p>2,467 kg</p>
                         </div>
                         <div className="highlightBox">
                             <svg
@@ -108,18 +122,75 @@ const Statistics = () => {
                                     stroke-width="3"
                                 />
                             </svg>
-                            <h4>TIME SPENT</h4>
-                            <p>46 hrs 12 min</p>
+                            <img src={calsBurnedIcon} alt="calsBurnedIcon" />
+
+                            <h4>CALS BURNED</h4>
+                            <p>4,987 cal</p>
                         </div>
                     </div>
                 </div>
 
                 <div className="profileSettings">
-                    <div className="landingWelcomeCont"></div>
+                    <div className="settingsBtns">
+
+                    <button className="myStatsBtn">
+                        <img src={statsIcon} alt="statsIcon" />
+                        <p>My Stats</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" fill="none">
+                            <path d="M1 1L11 9.26531L1 16" stroke="#180202" stroke-width="2"/>
+                        </svg>
+                    </button>
+
+                    <button className="accountSettingsBtn">
+                        <img src={accountSettingsIcon} alt="accountSettingsIcon" />
+                        <p>Account Settings</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" fill="none">
+                            <path d="M1 1L11 9.26531L1 16" stroke="#180202" stroke-width="2"/>
+                        </svg>
+                    </button>
+
+                    <button className="remindersBtn">
+                        <img src={remindersIcon} alt="remindersIcon" />
+                        <p>Reminders</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" fill="none">
+                            <path d="M1 1L11 9.26531L1 16" stroke="#180202" stroke-width="2"/>
+                        </svg>
+                    </button>
+
+                    <button className="privacyPolicyBtn">
+                        <img src={privacyPolicyIcon} alt="privacyPolicyIcon" />
+                        <p>Privacy Policy</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" fill="none">
+                            <path d="M1 1L11 9.26531L1 16" stroke="#180202" stroke-width="2"/>
+                        </svg>
+                    </button>
+
+                    <button className="tCBtn">
+                        <img src={tCIcon} alt="tCIcon" />
+                        <p>Terms & Conditions</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" fill="none">
+                            <path d="M1 1L11 9.26531L1 16" stroke="#180202" stroke-width="2"/>
+                        </svg>
+                    </button>
+
+                    <button className="updatesBtn">
+                        <img src={updatesIcon} alt="updatesIcon" />
+                        <p>Updates</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" fill="none">
+                            <path d="M1 1L11 9.26531L1 16" stroke="#180202" stroke-width="2"/>
+                        </svg>
+                    </button>
+
+                    <button className="notificationsBtn">
+                        <img src={notificationsIcon} alt="notificationsIcon" />
+                        <p>Notifications</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" fill="none">
+                            <path d="M1 1L11 9.26531L1 16" stroke="#180202" stroke-width="2"/>
+                        </svg>
+                    </button>
+                    
+                    </div>
                 </div>
-            </div>
-            <div>
-                <p>From API:</p>
             </div>
         </div>
     );
