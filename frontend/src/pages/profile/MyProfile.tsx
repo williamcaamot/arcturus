ab31c5c03a325b5b0d2cd37538436049fee91f8c
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-28 15:06:48 +0200

    trying to make contact with the api

--- frontend/src/pages/profile/MyProfile.tsx
+++ frontend/src/pages/profile/MyProfile.tsx
@@ -1,15 +1,31 @@
+import React, { useEffect, useState } from "react";
 import "./styles/myProfile.css";
 import backArrow from "../../../public/images/backArrow.png";
+import WorkoutService from "../../services/WorkoutService";
 
 const Statistics = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await WorkoutService.getWorkouts();
+            setWorkouts(response.data);
+        };
+
+        fetchWorkouts();
+    }, []);
+
     return (
         <div className="profileContainer">
             <div className="backBtn">
                 <button>
                     <img src={backArrow} alt="Back" />
                 </button>
+                <div>
+                    <p>From API:</p>
+                    <p>{workouts && JSON.stringify(workouts, null, 2)}</p>
+                </div>
             </div>
-
         </div>
     );
 };
