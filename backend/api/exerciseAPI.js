6d2260f4808b3a4f75055e590b13495a3efdfb60
williamdev <william-ca@live.no>
2024-05-29 16:13:14 +0200

    Cleanup

--- backend/api/exerciseAPI.js
+++ backend/api/exerciseAPI.js
@@ -6,27 +6,28 @@ export function exerciseAPI(db){
 
     router.get("", async(req, res)=>{
         try{
-            const collection = await db.collection("exercises")
+            const collection = db.collection("exercises")
 
             let limit = parseInt(req.query.limit) || 50;
             let offset = parseInt(req.query.offset) || 0;
 
-            if(limit > 50){limit = 50}
+            limit = Math.min(limit, 50);
 
             const result  = await collection.find().skip(offset).limit(limit).toArray();
 
-            res.json({result});
+            res.json(result);
         }catch (e) {
             console.log(e)
             res.sendStatus(500);
         }
     })
 
+
     router.get("/:id", async(req, res) => {
         try{
             const collection = await db.collection("exercises")
             const result = await collection.findOne({_id: new ObjectId(req.params.id)})
-            res.json({result});
+            res.json(result);
         }catch (e) {
             console.log(e)
             res.sendStatus(500);
