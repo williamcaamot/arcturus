f3e4d36c7816168f218ed105ded6e3e8596cd7da
williamdev <william-ca@live.no>
2024-05-29 15:41:38 +0200

    Created endpoint for exercises with pagination. Maximum items at once is 50.

--- backend/api/exerciseAPI.js
+++ backend/api/exerciseAPI.js
@@ -3,23 +3,22 @@ import express from "express";
 export function exerciseAPI(db){
     const router = express.Router();
 
-
     router.get("", async(req, res)=>{
         try{
             const collection = await db.collection("exercises")
-            const result  = await collection.find().toArray();
+
+            let limit = parseInt(req.query.limit) || 50;
+            let offset = parseInt(req.query.offset) || 0;
+
+            if(limit > 50){limit = 50}
+
+            const result  = await collection.find().skip(offset).limit(limit).toArray();
 
             res.json({result});
-
-
         }catch (e) {
-            console.log(e.message)
             console.log(e)
             res.sendStatus(500);
         }
-
-
     })
-
     return router;
 }
