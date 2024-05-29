f3c894c54d13931611f1c705d9d465c78b4f7d49
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2024-05-29 11:20:29 +0200

    added text boxxes and images

--- frontend/src/pages/sign_in/fill_personal_info/FillPersonalInfo.tsx
+++ frontend/src/pages/sign_in/fill_personal_info/FillPersonalInfo.tsx
@@ -1,13 +1,20 @@
- const FillPersonalInfo= () => {
+import "./styles/fillPersonalInfo.css";
+import Country from '../../../../public/images/personaliaCountry.png';
+import Age from '../../../../public/images/personaliaAge.png';
+import Weight from '../../../../public/images/personaliaWeight.png';
+import Height from '../../../../public/images/personaliaHeight.png';
+import Gender  from '../../../../public/images/personaliaGender.png';
+import Excercise from '../../../../public/images/personaliaExcercise.png';
 
 
 
-
+const FillPersonalInfo= () => {
 
     return (
         <div>
-            <div className="headerContent">
-                <h1>MY PROFILE</h1>
+            <div className="personaliaContainer">
+                <div className="headerBox">
+                <h1>GET STARTED</h1>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="225"
@@ -22,6 +29,57 @@
                         stroke-linecap="round"
                     />
                 </svg>
+                </div>
+                <div className="inputBoxes">
+
+                    <div className="inputBox">
+                        <img src={Country} alt="Country logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Name" />
+                    </div>
+
+                    <div className="inputBox">
+                        <img src={Country} alt="Country logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Email" />
+                    </div>
+
+                    <div className="inputBox">
+                         <img src={Country} alt="Country logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Password" />
+                    </div>
+
+                    <div className="inputBox">
+                    <img src={Country} alt="Country logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Country" />
+                    </div>
+
+                    <div className="inputBox">
+                        <img src={Age} alt="Age logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Age" />
+                    </div>
+
+                    <div className="inputBox">
+                        <img src={Weight} alt="Weight logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Weight" />
+                    </div>
+
+                    <div className="inputBox">
+                        <img src={Height} alt="Height logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Height" />
+                    </div>
+
+                    <div className="inputBox">
+                        <img src={Gender} alt="Gender logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Gender" />
+                    </div>
+
+                    <div className="inputBox">
+                        <img src={Excercise} alt="Excercise logo" className="personaliaLogo" />
+                        <input type="text" placeholder="Excercise" />
+                    </div>
+                    <div>
+                </div>
+               
+            </div>
             </div>
         </div>
     );
