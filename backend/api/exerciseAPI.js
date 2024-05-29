b43daa22cfd3eedae2120939c1a6ba155ef999f1
williamdev <william-ca@live.no>
2024-05-29 19:37:25 +0200

    Set up search API

--- backend/api/exerciseAPI.js
+++ backend/api/exerciseAPI.js
@@ -34,5 +34,51 @@ export function exerciseAPI(db){
         }
     })
 
+    router.get("/search/:term", async(req, res) => {
+        try{
+            const collection = db.collection("exercises");
+
+            let limit = parseInt(req.query.limit) || 50;
+            let offset = parseInt(req.query.offset) || 0;
+
+            const searchTerm = req.params.term;
+
+
+            let cursor = await collection.aggregate([
+                {
+                    $search: {
+                        index: "exercise_search_index",
+                        autocomplete: {
+                            query: `${searchTerm}`,
+                            path: "Exercise_Name",
+                            fuzzy: {
+                                maxEdits: 2,
+                                prefixLength: 2
+                            }
+                        }
+                    }
+                },{
+                    $facet: {
+                        "totalCount": [
+                            { $count: "count" }
+                        ],
+                        "results": [
+                            { $skip: offset },
+                            { $limit: limit }
+                        ]
+                    }
+                }
+            ]);
+
+            const result = await cursor.toArray();
+
+            res.json(result);
+        }catch (e) {
+            console.log(e)
+            res.sendStatus(500);
+        }
+    })
+
+
     return router;
 }
