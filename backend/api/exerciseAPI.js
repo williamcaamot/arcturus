a25f9d98669b7d88f42fb3418c6d9518a121dba3
williamdev <william-ca@live.no>
2024-05-29 12:08:07 +0200

    Change to default db so not need to select. Remove .finally on server start to prevent mongodb colleciton to close

--- backend/api/exerciseAPI.js
+++ backend/api/exerciseAPI.js
@@ -1,15 +1,25 @@
 import express from "express";
 
-export function exerciseAPI(mongoClient){
+export function exerciseAPI(db){
     const router = express.Router();
 
 
     router.get("", async(req, res)=>{
-        res.json({message:"Hello world"});
+        try{
+            const collection = await db.collection("exercises")
+            const result  = await collection.find().toArray();
+
+            res.json({result});
+
+
+        }catch (e) {
+            console.log(e.message)
+            console.log(e)
+            res.sendStatus(500);
+        }
+
+
     })
-
-
-
 
     return router;
 }
