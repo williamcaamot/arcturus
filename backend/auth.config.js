64a97756eb98cb10e47e60f8a88d79fe59345da1
williamdev <william-ca@live.no>
2024-05-30 10:19:05 +0200

    Authjs working

--- backend/auth.config.js
+++ backend/auth.config.js
@@ -1,4 +1,13 @@
 import Google from "@auth/express/providers/google"
+import dotenv from "dotenv";
+import {MongoDBAdapter} from "@auth/mongodb-adapter";
+import {MongoClient} from "mongodb";
+
+dotenv.config()
+
+let client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+let clientPromise = client.connect();
+
 
 export const authConfig = {
     trustHost: true,
@@ -6,6 +15,10 @@ export const authConfig = {
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            //redirect_uris: [
+            //    'http://localhost:3000/api/auth/callback/google'     // <-- problem here
+            //],
         }),
     ],
+    adapter: MongoDBAdapter(clientPromise, {databaseName: "flexr"}),
 }
