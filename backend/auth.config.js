80f12f017a245e64547b271e72ae20bc2d6d9c9f
williamdev <william-ca@live.no>
2024-05-30 09:27:59 +0200

    Setup auth.js

---
+++ backend/auth.config.js
@@ -0,0 +1,11 @@
+import Google from "@auth/express/providers/google"
+
+export const authConfig = {
+    trustHost: true,
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+}
