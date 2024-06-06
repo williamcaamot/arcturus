da939079540452c33da90af5c59c01ad1059192f
Antonio Acosta <136067442+antonioacostaa@users.noreply.github.com>
2024-06-06 15:35:24 +0200

    Small fix for save button, requiring exercises to be able to save

--- frontend/src/pages/workouts/workouts/CreateWorkout.tsx
+++ frontend/src/pages/workouts/workouts/CreateWorkout.tsx
@@ -30,7 +30,7 @@ const CreateWorkout = () => {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    workoutName: workoutName,
+                    workoutName: workoutName.length === 0 ? 'New Workout' : workoutName,
                     description: workoutDescription,
                     image: workoutImgUrl,
                     exercises: newWorkoutExercises,
@@ -150,17 +150,19 @@ const CreateWorkout = () => {
                     </div>
                     <Navigation />
                     <div className='floatingBtnContainer'>
+                        
                         <button
                             role='button'
+                            disabled={newWorkoutExercises.length === 0}
                             tabIndex={0}
                             onClick={() => createWorkout()}
                             className='floatingBtn'
-                            style={{ backgroundColor: '#246656', color: 'white' }}>
+                            style={newWorkoutExercises.length !== 0 ? { backgroundColor: '#246656', color: 'white' } : {backgroundColor: 'gray'}}>
                             Save{' '}
                             <svg xmlns='http://www.w3.org/2000/svg' width='21' height='25' viewBox='0 0 21 25' fill='none'>
                                 <path
                                     d='M7.87488 16.8437L4.83863 13.2292C4.67503 13.0344 4.45313 12.925 4.22176 12.925C3.99039 12.925 3.76849 13.0344 3.60488 13.2292C3.44128 13.4239 3.34937 13.6881 3.34937 13.9635C3.34937 14.0999 3.37193 14.235 3.41577 14.361C3.45961 14.487 3.52387 14.6015 3.60488 14.6979L7.26238 19.0521C7.60363 19.4583 8.15488 19.4583 8.49613 19.0521L17.7536 8.03124C17.9172 7.83647 18.0091 7.57231 18.0091 7.29686C18.0091 7.02142 17.9172 6.75726 17.7536 6.56249C17.59 6.36772 17.3681 6.2583 17.1368 6.2583C16.9054 6.2583 16.6835 6.36772 16.5199 6.56249L7.87488 16.8437Z'
-                                    fill='white'
+                                    fill={newWorkoutExercises.length !== 0 ? 'white' : 'darkgray'}
                                 />
                             </svg>
                         </button>
